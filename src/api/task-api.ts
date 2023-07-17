6ad5e8e6660a70cbdbf653d90661573914a5cb89
avkotau <kotov.alexander1@mail.ru>
2023-07-17 11:24:20 +0200

    feat: add storybook, all changes to tasks using the API

---
+++ src/api/task-api.ts
@@ -0,0 +1,60 @@
+import axios from 'axios'
+
+const instance = axios.create(
+    {
+        baseURL: `https://social-network.samuraijs.com/api/1.1/`,
+        withCredentials: true,
+
+    }
+)
+
+type ItemType = {
+    addedDate: string
+    deadline: boolean
+    description: boolean
+    id: string
+    order: number
+    priority: number
+    startDate: boolean
+    status: number
+    title: string
+    todoListId: string
+}
+
+type TaskType = {
+    items: ItemType[]
+    totalCount: number
+    error: boolean
+}
+
+type ResponseType<T> = {
+    data: T
+    fieldsErrors: []
+    messages: []
+    resultCode: number
+}
+
+
+export const taskAPI = {
+
+    getTasks(todolistId: string) {
+        return instance.get<TaskType>(`todo-lists/${todolistId}/tasks`)
+    },
+
+    createTask(todolistId: string, title: string) {
+        return instance.post<ResponseType<{items: ItemType}>>(`todo-lists/${todolistId}/tasks`, {title})
+    },
+
+    deleteTask(todolistId: string, taskId: string) {
+        return instance.delete<ResponseType<{}>>(`todo-lists/${todolistId}/tasks/${taskId}`)
+
+    },
+
+    updateTask(todolistId: string, taskId: string, title: string) {
+        return instance.put<ResponseType<{items: ItemType}>>(
+            `todo-lists/${todolistId}/tasks/${taskId}`,
+            {title}
+        )
+    },
+}
+
--- src/stories/tasks-api.stories.tsx
+++ src/stories/tasks-api.stories.tsx
@@ -1,29 +1,33 @@
 import React, { useEffect, useState } from 'react'
-import { todolistAPI } from "../api/todolist-api";
+import { taskAPI } from "../api/task-api";
 
 export default {
     title: 'API'
 }
 
-export const GetTodolists = () => {
+export const GetTasks = () => {
     const [state, setState] = useState<any>(null)
 
     useEffect(() => {
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
-        todolistAPI.getTodolists()
+
+        const todolistId = '3b0304fc-63df-4ddb-801a-f44264e6222e'
+        taskAPI.getTasks(todolistId)
             .then((res) => {
+                console.log(res.data)
                 setState(res.data)
             })
     }, [])
     return <div>{JSON.stringify(state)}</div>
 }
-export const CreateTodolist = () => {
+export const CreateTask = () => {
     const [state, setState] = useState<any>(null)
 
     useEffect(() => {
-        const title = 'REEEEEACCCCTTTT'
-        todolistAPI.createTodolist(title)
+        const Title = 'WWWWWWWWWWWWWW'
+        const todolistId = '3b0304fc-63df-4ddb-801a-f44264e6222e'
+        taskAPI.createTask(todolistId, Title)
 
             .then((res) => {
 
@@ -35,27 +39,29 @@ export const CreateTodolist = () => {
 
     return <div>{JSON.stringify(state)}</div>
 }
-export const DeleteTodolist = () => {
+export const DeleteTask = () => {
     const [state, setState] = useState<any>(null)
 
     useEffect(() => {
-        const todolistId = '1056cb3d-29aa-4673-84ba-67a35efc54b4'
-        todolistAPI.deleteTodolist(todolistId)
+        const todolistId = '3b0304fc-63df-4ddb-801a-f44264e6222e'
+        const taskId = 'dfbdd947-7845-4f63-a221-e8747bb4e0ed'
+        taskAPI.deleteTask(todolistId, taskId)
             .then((res) => {
 
                 setState(res.data)
-            })
+            }).catch(err => err)
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
 }
-export const UpdateTodolistTitle = () => {
+export const UpdateTaskTitle = () => {
     const [state, setState] = useState<any>(null)
 
     useEffect(() => {
-        const todolistId = '1221d8b1-7abe-444b-909c-bdfa5c4749f7'
-        const title = 'Redux'
-        todolistAPI.updateTodolist(todolistId, title)
+        const todolistId = '3b0304fc-63df-4ddb-801a-f44264e6222e'
+        const taskId = '893beaef-8b35-462a-abea-5190cb1f85ee'
+        const title = 'JS'
+        taskAPI.updateTask(todolistId, taskId, title)
             .then((res) => {
                 setState(res.data)
             })
